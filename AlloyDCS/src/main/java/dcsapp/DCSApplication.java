e6ac8d6b9a695de09d9a2e2268ccf75cc3574758
Sebastian Fuchs <fuchs.se.de@gmail.com>
2015-04-07 15:26:43 +0200

    added main application starting point

---
+++ AlloyDCS/src/main/java/dcsapp/DCSApplication.java
@@ -0,0 +1,16 @@
+package dcsapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DCSApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO load DCSRootPane as root node
+		
+		// TODO one extra stage for every screen
+		
+	}
+
+}
